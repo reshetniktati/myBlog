3ede0324e6990993ecff3bc794e5dfc2852b94df
tresh@softserveinc.com <tresh@softserveinc.com>
2021-09-13 16:46:27 +0300

    gitignore

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,10 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\SubscriptionController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 Auth::routes();
 
 Route::get('/', [WelcomeController::class, 'index']);
